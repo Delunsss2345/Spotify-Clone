04fd210e710efedd8c783ffe413627e1fa2b827a
Delunsss2345 <huypham1459@gmail.com>
2025-07-28 11:11:12 +0700

    build fe -> be

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: path.resolve(__dirname, "../backend/public/dist"),
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
